4850abfc5920c5ee0841f7616c30b6158fe28a56
vanisrimegala <30829842+vanisrimegala@users.noreply.github.com>
2018-02-08 08:26:22 +0000

    Update and rename 31.c to 32.c

--- 32.c
+++ 32.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
-
 int main()
 {
-	int count=0,i,w=0;;
+	int count=0,i=0;
 	char ch[100];
 	printf("Enetr the string \n");
 	scanf("%s",ch);
-	for(i=0;ch[i]!=NULL;i++)
+	while(ch[i]=='\0')
 	{
-	
-	if(ch[i]==' ')
+	if(ch[i]==" ")
 	{
-	w=w+1;	
+count++;
 	}
+		i++;
 	}
 	
 	printf("%d \n",count);
-	printf("%d",w+1);
+	
 	return 0;
 }
