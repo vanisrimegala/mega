70a9f295fa667265b7ca76ad4c29f02b07641fe2
vanisrimegala <30829842+vanisrimegala@users.noreply.github.com>
2018-02-13 15:12:58 +0530

    Update 57.c

--- 57.c
+++ 57.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main()
 {
-	int t1,t2,temp;
+	int t3,t4,temp;
 	printf("Enter the numbers\n");
-	scanf("%d%d",&t1,&t2);
-	printf("%d \t %d \n",t1,t2);
+	scanf("%d%d",&t3,&t4);
+	printf("%d \t %d \n",t3,t4);
 	{
-	temp=t1;
-	t1=t2;
-	t2=temp;
+	temp=t3;
+	t3=t4;
+	t4=temp;
 	}
-	printf("%d\t%d\t",t1,t2);
+	printf("%d\t%d\t",t3,t4);
 	return 0;
 }
