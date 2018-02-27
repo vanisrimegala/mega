26c818cef8780f577e0717d873866370d12cd572
vanisrimegala <30829842+vanisrimegala@users.noreply.github.com>
2018-02-27 16:32:50 +0530

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+ int m,n,add;
+ printf("Enter number : ");
+ scanf("%d%d",&m,&n);
+ add=m+n;
+   if(add%2==0)
+   {
+    printf("\n yes");
+   }
+   else
+   {
+       printf("\n no");
+   }
+ return 0;
+}
