771aed563034ed6059c5e23d21e3825e90632581
vanisrimegala <30829842+vanisrimegala@users.noreply.github.com>
2018-03-02 16:33:20 +0530

    Update 79perfectsquare.c

--- 79perfectsquare.c
+++ 79perfectsquare.c
@@ -1,17 +1,25 @@
 #include<stdio.h>
 int main() 
 {
-  int n,r,s;
+  int n,r,s,i,count=0;;
   printf("Enter the numbers \n");
   scanf("%d%d",&n,&s);
  r=n*s;
-  if(n==s)
-  {
-          printf("yes \n");
-  }
-  else
-  {
-      printf("not a perfect square");
-  }
+for(i=n;i<=s;i++)
+{
+    if(r==i*i)
+    {
+          count=count+1;;
+    }
+}
+if(count==1)
+{
+    printf("Yes \n");
+}
+else
+{
+    printf("No");
+}
   return 0;
 }
+
