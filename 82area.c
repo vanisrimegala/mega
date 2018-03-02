8b36e37532d88d9665f8c4ba3179b1d563b37c7c
vanisrimegala <30829842+vanisrimegala@users.noreply.github.com>
2018-03-02 16:54:14 +0530

    Create 82area.c

---
+++ 82area.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main() 
+{
+  float l,b,a;
+  printf("Enter the length and breadth \n");
+  scanf("%f%f",&l,&b);
+  a=l*b;
+    printf("%f \n",a);
+  return 0;
+}
