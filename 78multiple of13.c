cb95c79021604310906caf457e061b7b7aba3e86
vanisrimegala <30829842+vanisrimegala@users.noreply.github.com>
2018-03-02 16:39:20 +0530

    Create 78multiple of13.c

---
+++ 78multiple of13.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main() 
+{
+  int n,r,s,i,count=0;;
+  printf("Enter the number \n");
+  scanf("%d",&n);
+    if(n%13==0)
+    {
+    printf("Yes \n");
+    }
+ else
+{
+    printf("No");
+}
+  return 0;
+}
