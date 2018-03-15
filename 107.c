31f5f8c5e2917fa2a767fcc0c27eec194a1e6143
vanisrimegala <30829842+vanisrimegala@users.noreply.github.com>
2018-03-15 16:20:51 +0530

    Create 107.c

---
+++ 107.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+   int a,b,c,d,e;
+   printf("the  numbers \n");
+   scanf("%d%d%d",&a,&b,&c);
+   d=a*b;
+   e=d/c;
+   printf("%d",e);
+   return 0;
+}
