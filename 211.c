7281708664116be581bd01db01a943063e27f590
vanisrimegala <30829842+vanisrimegala@users.noreply.github.com>
2018-03-16 15:49:53 +0530

    Create 211.c

---
+++ 211.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+   int b,h,area;
+   printf("Enter base and height \n");
+   scanf("%d%d",&b,&h);
+   area=h*b/2;
+   printf("Area of triangle is %d \n",area);
+   return 0;
+}
+   
+   
